55809846f075096713e7dfc7bbb0f7594e9ce97e
szymchydz <147948466+szymchydz@users.noreply.github.com>
2024-08-11 17:20:22 +0200

    Add files via upload

---
+++ Transaction.h
@@ -0,0 +1,11 @@
+#pragma once
+
+struct Transaction
+{
+  int id = 0;
+  int userId = 0;
+  int date = 0;
+  string item = "";
+  double amount = 0.0;
+
+};
---
+++ User.h
@@ -0,0 +1,13 @@
+#pragma once
+
+
+struct User
+{
+  int id = 0;
+  string name = "";
+  string surname = "";
+  string login = "";
+  string password = "";
+
+};
+
