24bf357c4fc223277213eee1e68439f5ac505dfd
Szymon Chydziński <szymon.chydzinski.programista@gmail.com>
2024-10-13 19:16:30 +0200

    TransactionFile class update

--- TransactionFile.cpp
+++ TransactionFile.cpp
@@ -3,7 +3,9 @@
 void TransactionFile::loadCurrentId() {
 
     if (xmlTransactions.Load(getFileName())) {
-        for (const string& elemName : {"Incomes", "Expenses"}) {
+        for (const string& elemName : {
+                    "Incomes", "Expenses"
+                }) {
             if (xmlTransactions.FindElem(elemName)) {
                 xmlTransactions.IntoElem();
                 while (xmlTransactions.FindElem("Transaction")) {
@@ -11,9 +13,9 @@ void TransactionFile::loadCurrentId() {
                     if (xmlTransactions.FindElem("TransactionId")) {
                         string elemValue = xmlTransactions.GetElemContent();
                         try {
-                            int id = stoi(elemValue); // Convert string to int
+                            int id = stoi(elemValue);
                             if (id > currentTransactionId) {
-                                currentTransactionId = id; // Update if id is greater
+                                currentTransactionId = id;
                             }
                         } catch (const exception& e) {
                             cerr << "Invalid TransactionId encountered: " << e.what() << endl;
@@ -28,33 +30,29 @@ void TransactionFile::loadCurrentId() {
 }
 
 bool TransactionFile::addTransactionToFile(Transaction &transaction, const Type &type) {
- string fileName = getFileName();
-    cout << "Sprawdzam plik: " << fileName << std::endl;
 
-    // Sprawdzamy, czy plik istnieje
+    string fileName = getFileName();
+
     ifstream file(fileName);
     bool fileExists = file.good();
-    file.close(); // Zamknij plik po sprawdzeniu
+    file.close();
 
     if (!fileExists) {
-        // Tworzymy nowy dokument XML, jeśli plik nie istnieje
         xmlTransactions.SetDoc("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\r\n<Transactions></Transactions>");
         cout << "Utworzono nowy plik XML." << endl;
     } else {
         cout << "Wczytano istniejący plik XML." << endl;
     }
 
-    // Znajdujemy sekcję główną "Transactions"
     if (!xmlTransactions.FindElem("Transactions")) {
         xmlTransactions.AddElem("Transactions");
         cout << "Dodano sekcję Transactions." << endl;
     }
 
-    xmlTransactions.FindElem("Transactions"); // Wchodzimy do sekcji Transactions
+    xmlTransactions.FindElem("Transactions");
 
-    std::string transactionType;
+    string transactionType;
 
-    // Ustal typ transakcji
     if (type == INCOME) {
         transactionType = "Incomes";
     } else if (type == EXPENSE) {
@@ -64,60 +62,14 @@ bool TransactionFile::addTransactionToFile(Transaction &transaction, const Type 
         return false;
     }
 
-    // Znajdź lub dodaj sekcję Incomes/Expenses
     if (!xmlTransactions.FindElem(transactionType)) {
         xmlTransactions.AddElem(transactionType);
         cout << "Dodano sekcję: " << transactionType << endl;
     }
 
-    // Wchodzimy do sekcji (Incomes lub Expenses)
     xmlTransactions.IntoElem();
 
-    // Dodajemy nową transakcję na końcu sekcji
-    xmlTransactions.AddElem("Transaction");
-    xmlTransactions.IntoElem();  // Wejdź do nowo dodanego elementu "Transaction"
-    xmlTransactions.AddElem("TransactionId", transaction.id);
-    xmlTransactions.AddElem("UserId", transaction.userId);  // Dodajemy UserId
-    xmlTransactions.AddElem("Date", transaction.date);
-    xmlTransactions.AddElem("Item", transaction.item);
-    xmlTransactions.AddElem("Amount", transaction.amount);
-    xmlTransactions.OutOfElem();  // Wychodzimy z "Transaction"
-    xmlTransactions.OutOfElem();  // Wychodzimy z sekcji (Incomes/Expenses)
 
-    // Zapisz zmiany w pliku
-    if (xmlTransactions.Save(getFileName())) {
-        cout << "Dane zostały zapisane." << endl;
-        currentTransactionId++;  // Zwiększ ID dla nowej transakcji
-        return true;
-    } else {
-        cout << "Nie udało się zapisać pliku." << endl;
-        return false;
-    }
-}
-/*bool TransactionFile::addTransactionToFile(Transaction &transaction, const Type &type) {
-
-    bool fileExists = xmlTransactions.Load(getFileName());
-
-    if (!fileExists) {
-        xmlTransactions.SetDoc("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\r\n");
-    }
-
-    if (type == INCOME) {
-        if (!xmlTransactions.FindElem("Incomes")) {
-            xmlTransactions.AddElem("Incomes");
-        }
-        xmlTransactions.FindElem("Incomes");
-    } else if (type == EXPENSE) {
-        if (!xmlTransactions.FindElem("Expenses")) {
-            xmlTransactions.AddElem("Expenses");
-        }
-        xmlTransactions.FindElem("Expenses");
-    } else {
-        cout << "Nieznany typ transakcji." << endl;
-        return false;
-    }
-
-    xmlTransactions.IntoElem();
     xmlTransactions.AddElem("Transaction");
     xmlTransactions.IntoElem();
     xmlTransactions.AddElem("TransactionId", transaction.id);
@@ -129,14 +81,14 @@ bool TransactionFile::addTransactionToFile(Transaction &transaction, const Type 
     xmlTransactions.OutOfElem();
 
     if (xmlTransactions.Save(getFileName())) {
-        cout << "Dane zostaly zapisane." << endl;
+        cout << "Dane zostały zapisane." << endl;
         currentTransactionId++;
         return true;
     } else {
-        cout << "Nie udalo sie zapisac danych." << endl;
+        cout << "Nie udało się zapisać pliku." << endl;
         return false;
     }
-}*/
+}
 
 vector<Transaction> TransactionFile::loadTransactionFromFile(int loggedInUserId) {
 
@@ -147,20 +99,19 @@ vector<Transaction> TransactionFile::loadTransactionFromFile(int loggedInUserId)
 
     if (fileExists) {
         xmlTransactions.ResetPos();
-        xmlTransactions.FindElem();  // Root element <Transactions>
+        xmlTransactions.FindElem();
 
-        // Load both Incomes and Expenses for the specific user
-        for (const string& elemName : {"Incomes", "Expenses"}) {
+        for (const string& elemName : {
+                    "Incomes", "Expenses"
+                }) {
             if (xmlTransactions.FindElem(elemName)) {
                 xmlTransactions.IntoElem();
 
-                // Now find the specific section for the loggedInUserId
                 if (xmlTransactions.FindElem("UserId_" + to_string(loggedInUserId))) {
                     xmlTransactions.IntoElem();
 
-                    // Iterate through all transactions for this user
                     while (xmlTransactions.FindElem("Transaction")) {
-                        xmlTransactions.IntoElem();  // Enter the <Transaction> element
+                        xmlTransactions.IntoElem();
 
                         xmlTransactions.FindChildElem("TransactionId");
                         transaction.id = atoi(xmlTransactions.GetChildData().c_str());
@@ -179,11 +130,11 @@ vector<Transaction> TransactionFile::loadTransactionFromFile(int loggedInUserId)
 
                         transactions.push_back(transaction);
 
-                        xmlTransactions.OutOfElem();  // Exit the <Transaction> element
+                        xmlTransactions.OutOfElem();
                     }
-                    xmlTransactions.OutOfElem();  // Exit UserId section
+                    xmlTransactions.OutOfElem();
                 }
-                xmlTransactions.OutOfElem();  // Exit Incomes/Expenses
+                xmlTransactions.OutOfElem();
             }
         }
     } else {
